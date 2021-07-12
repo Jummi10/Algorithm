1b46709792e82dd62c1a636eb4c94acc17978362
jummi <98qkrwjdal@naver.com>
2021-07-13 03:23:55 +0900

    2725 success

--- baekjoon/정수론/보이는_점의_개수.java
+++ baekjoon/정수론/보이는_점의_개수.java
@@ -4,30 +4,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 2725 fail - 시간초과
+// 2725 success
 public class 보이는_점의_개수 {
     private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+    private static final int MAX = 1001;
+    private static int[] dp = new int[MAX];
 
     public static void main(String[] args) throws IOException {
-        int tc = input();
+        initializeDP(); //  n*n일 때 개수
 
+        int tc = input();
         for (int i = 0; i < tc; i++) {
             int n = input();
-            int count = 3;  // (0,1) (1,0) (1,1)
-
-            for (int x = 1; x <= n; x++) {
-                for (int y = 1; y < x; y++) { // y=x 아래 구역만 살펴보고 2배
-                    if (gcd(x, y) == 1) {
-                        count += 2;
-                    }
-                }
-            }
-            System.out.println(count);
+            System.out.println(dp[n]);
         }
+
     }
 
     private static int input() throws IOException {
         return Integer.parseInt(READER.readLine().trim());
+    }
+
+    private static void initializeDP() {
+        dp[1] = 3;  // (0,1) (1,0) (1,1)
+        for (int i = 2; i < MAX; i++) {
+            int count = 0;
+            for (int x = 1; x < i; x++) {
+                if (gcd(x, i) == 1) {   // y/x 기울기가 더 이상 약분되지 않는다면
+                    count += 2; // // y=x 윗 구역에서 x축과 평행한 선만 살펴보고 2배
+                }
+            }
+            dp[i] = dp[i - 1] + count;
+        }
     }
 
     private static int gcd(int a, int b) { // 최대공약수
