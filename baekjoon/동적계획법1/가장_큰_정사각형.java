cd9a76b60ef43fdf7accd273d21daa68538dfe24
jummi <98qkrwjdal@naver.com>
2021-07-16 14:26:02 +0900

    1915 success

--- baekjoon/동적계획법1/가장_큰_정사각형.java
+++ baekjoon/동적계획법1/가장_큰_정사각형.java
@@ -3,25 +3,30 @@ package practice.baekjoon.동적계획법1;
 import java.io.*;
 import java.util.StringTokenizer;
 
-// 1915 fail
+// 1915 success
 public class 가장_큰_정사각형 {
+    // fail 원인: 입력이 모두 0인 경우를 고려하면 width 초기화를 1로 시키면 안 된다!
     private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
     private static final BufferedWriter WRITER = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = getLine();
         int row = parseInt(st), column = parseInt(st);  // 1<= r,c <= 1000
-        int width = 1;
+        int width = 0;
+
         if (row == 1 || column == 1) {
+            width = 1;
             printResult(width);
             return;
         }
-        int[][] table = new int[row + 1][column + 1];
+        int[][] table = new int[row + 1][column + 1];   // r=0, c=0 일 경우 0을 padding
 
         for (int r = 1; r <= row; r++) {
             String input = getLine().nextToken();
+
             for (int c = 1; c <= column; c++) {
-                table[r][c] = input.charAt(c - 1) - '0';
+                table[r][c] = input.charAt(c - 1) - '0';    // 입력
+
                 if (table[r][c] == 1) {
                     // 오른쪽, 위쪽, 오른쪽 위 대각선 중 가장 작은 값
                     // 1. 셋 다 1이면 min = 1이니까 정사각형 완성
@@ -32,7 +37,8 @@ public class 가장_큰_정사각형 {
             }
         }
 
-        /*for (int r = 1; r <= row; r++) {
+        /* 확인
+        for (int r = 1; r <= row; r++) {
             for (int c = 1; c <= column; c++) {
                 System.out.print(table[r][c] + " ");
             }
